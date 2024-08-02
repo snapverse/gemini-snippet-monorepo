c5e1c836fbc2c487e7eb6427e879d2d70f08e7f0
Simón Villafañe <simonvillafanee@gmail.com>
2024-08-02 01:29:10 -0300

    feat(Root): config & declarations adjustments

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,13 +1,21 @@
 declare const MONOREPO_ROOT: string;
 
-export namespace GoogleSearchEngine {
+declare namespace GoogleSearchEngine {
   export interface Source {
     pageTitle: string;
     href: string;
   }
 
   export interface Content {
-		title: string
-		description: string
-	}
+    title: string;
+    description: string;
+  }
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'testing' | 'staging';
+    DB_MEMORY: string;
+    GEMINI_API_KEY?: string;
+  }
 }
